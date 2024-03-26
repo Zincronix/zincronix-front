251d9399d900a08b6aed57ce26622cb7ca738164
Erick <maumbeltran@gmail.com>
2024-03-26 00:57:03 -0400

    services

--- src/services/materiaService.js
+++ src/services/materiaService.js
@@ -3,6 +3,7 @@ import api from "./api";
 export const getMaterias = async () => {
   try {
     const response = await api.get("/materias");
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error al obtener materias: ", error);
