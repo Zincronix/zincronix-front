5933c6f2eef4268b3aceab0aa2e91d27794ba6ca
Ailiez <dennis-galindo1@hotmail.com>
2024-03-26 22:38:58 -0400

    Validaciones de campo añadidas

--- src/pages/RegistrarAmbiente.jsx
+++ src/pages/RegistrarAmbiente.jsx
@@ -6,7 +6,14 @@ export function RegistrarAmbiente () {
   const [nameError, setNameError] = useState('')
   const [capacityError, setCapacityError] = useState('')
   const [descriptionError, setDescriptionError] = useState('')
+  const [imageError, setImageError] = useState('')
+
+  var flag = false
   function handleFileUpload(event) {
+    const imagen = event.target.files[0]
+    if (imagen.size > 100000){
+      setImageError("El tamaño de la imagen no debe ser mayor a 100kb")
+    }
     setImage(event.target.files[0])
   }
 
@@ -14,50 +21,50 @@ export function RegistrarAmbiente () {
     event.preventDefault()
 
     const {nombre, capacidad, descripcion} = Object.fromEntries(new FormData(event.target))
-    
-    const nombreLongitud = nombre.length
-    
-    const specialChar = new RegExp('[^\p{L}\s._0-9\-]+')
+    const nombreLongitud = (nombre.trim()).length
+    const imagen = new Image();
+
+    const specialChar = new RegExp('[^a-zA-Z0-9]^\s')
+    flag = false;
 
     if(specialChar.test(nombre)){
       setNameError("No se permiten caracteres especiales")
-      return
-    }
-
-    const emptySpace = new RegExp('\s')
-
-    if (emptySpace.test(nombre)){
-      setNameError("No se permiten espacios")
-      return
+      flag = true;
     }
 
     if (nombreLongitud  === 0) {
       setNameError("El campo nombre es obligatorio")
-      return
+      flag = true;
     }
 
     if (nombreLongitud > 15) {
       setNameError("Solo se aceptan 15 caracteres")
-      return
+      flag = true;
     }
     
     if (!capacidad) {
       setCapacityError("El campo capacidad es obligatorio")
-      return
+      flag = true;
     }
 
     if (capacidad > 1000) {
       setCapacityError("No se admite números mayores a 1000")
-      return
+      flag = true;
     }
 
     if (descripcion.length > 500){
       setDescriptionError("Solo se admite como máximo 500 caracteres")
-      return
+      flag = true;
     }
+
+    if (flag) return
     
     alert("Desea enviar el formulario?")    
     const fd = new FormData(event.target)
+
+    setNameError('')
+    setCapacityError('')
+    setDescriptionError('')
     
   }
 
@@ -69,13 +76,17 @@ export function RegistrarAmbiente () {
           <div className="fields">
             <div className="form-field field-1">
               <label className="form-label">Nombre: <span>*</span></label>
-              <input name="nombre" type="text" className="form-input" />
-              <p>{nameError}</p>
+              <div className="form-input-container">
+                <input name="nombre" type="text" className="form-input" />
+                <p>{nameError}</p>
+              </div>
             </div>
             <div className="form-field field-2">
               <label className="form-label">Capacidad: <span>*</span></label>
-              <input name="capacidad" type="number" className="form-input" min={0}/>
-              <p>{capacityError}</p>
+              <div className="form-input-container">
+                <input name="capacidad" type="number" className="form-input" min={0}/>
+                <p>{capacityError}</p>
+              </div>
             </div>
             <div className="form-field form-condicion field-3">
               <label className="form-label">Condiciones Especiales:</label>
@@ -88,14 +99,18 @@ export function RegistrarAmbiente () {
             </div>
             <div className="form-field field-4">
               <label className="form-label">Descripción:</label>
-              <textarea name="descripcion" cols="38" rows="8"></textarea>
-              <p>{descriptionError}</p>
+              <div className="form-input-container">
+                <textarea className="form-input" name="descripcion" cols="38" rows="8"></textarea>
+                <p>{descriptionError}</p>
+              </div>
             </div>
           </div>
 
           <div className="image">
             <div className="preview">
-              <input type="file" name="imagen" onClick={handleFileUpload} multiple={false} />
+              <label htmlFor="imagen">Subir Foto</label>
+              <input type="file" id="imagen" name="imagen" onChange={handleFileUpload} multiple={false} accept="image/png, image/jpeg, image/jpg" />
+              <p>{imageError}</p>
             </div>
           </div>
         </section>
